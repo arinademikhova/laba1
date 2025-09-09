dbf0e42dc0684b6ad236335168b8968c5cffbd24
arina <arinademikhova@gmail.com>
2025-09-09 10:20:49 +0300

    DemoSlider

--- app/src/main/java/com/example/composedemo/MainActivity.kt
+++ app/src/main/java/com/example/composedemo/MainActivity.kt
@@ -7,11 +7,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.composedemo.ui.theme.ComposeDemoTheme
 
@@ -37,6 +39,15 @@ fun DemoText(message: String, fontSize: Float) {
         fontWeight = FontWeight.Bold
     )
 }
+@Composable
+fun DemoSlider(sliderPosition: Float, onPositionChange: (Float) -> Unit ) {
+    Slider(
+        modifier = Modifier.padding(10.dp),
+        valueRange = 20f..38f,
+        value = sliderPosition,
+        onValueChange = { onPositionChange(it) }
+    )
+}
 
 @Preview
 @Composable
@@ -45,3 +56,4 @@ fun DemoTextPreview() {
         DemoText(message = "Welcome to Android", fontSize = 12f)
     }
 }
+
